98b2d3412d2750deb533e88b615219266777f9d0
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-27 18:50:53 +0900

    add server

---
+++ server.js
@@ -0,0 +1,17 @@
+const server = require("ws").Server;
+const s = new server({ 
+    host: "localhost",
+    port: 5001 });
+
+console.log(`start listening... : ${s.host}:${s.port}`)
+s.on("connection", ws => {
+    ws.on("message", message => {
+        console.log("Received: " + message);
+
+        if (message === "hello") {
+            ws.send("hello from server");
+        } else if (message === "test") {
+            ws.send("test from server")
+        }
+    });
+});
