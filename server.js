b123c30d3076fec2e7ff03a126c0a0d90808158c
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-27 19:19:07 +0900

    format ws

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const server = require("ws").Server;
-const s = new server({ 
-    host: "localhost",
-    port: 5001 });
+const s = new server({
+  host: "localhost",
+  port: 5001,
+});
 
-console.log(`start listening... : ${s.host}:${s.port}`)
-s.on("connection", ws => {
-    ws.on("message", message => {
-        console.log("Received: " + message);
+console.log(`start listening... : ${s.host}:${s.port}`);
+s.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log("Received: " + message);
 
-        if (message === "hello") {
-            ws.send("hello from server");
-        } else if (message === "test") {
-            ws.send("test from server")
-        }
-    });
+    if (message === "hello") {
+      ws.send("hello from server");
+    } else if (message === "test") {
+      ws.send("test from server");
+      ws.send({ message: "from_server", error: null });
+    }
+  });
 });
