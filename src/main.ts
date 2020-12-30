d8303e24e9d414646f7ac4cd54a032f1ca88c245
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 07:01:13 +0900

    delete comment

--- src/main.ts
+++ src/main.ts
@@ -61,38 +61,6 @@ const eventListener = (message_e: MessageEvent<string>) => {
   }
 };
 
-// sock.addEventListener("message", (message_e: MessageEvent<string>) => {
-//   console.log("メッセージ : " + `${message_e.data}`);
-//   const data = JSON.parse(message_e.data);
-//   if (data.type === "one_word") {
-//     const pos = data.pos;
-//     const word: string = data.word;
-//     console.log(`one word req: pos: ${pos}, word: ${word}`);
-//     const x = (pos % 32) * 16;
-//     const y = Math.floor(pos / 32);
-//     console.log(`x: ${x}, y: ${y}`);
-//     for (var i = 0; i < 16; i++) {
-//       const char = word[i];
-//       if (char === "O") {
-//         ctx.fillStyle = "white";
-//         ctx.fillRect(x + i, y, 1, 1);
-//       } else {
-//         ctx.fillStyle = "black";
-//         ctx.fillRect(x + i, y, 1, 1);
-//       }
-//     }
-//     console.log(`fill ends.`);
-//   }
-// });
-
-// sock.addEventListener("close", (e) => {
-//   console.log("接続が閉じられたときに呼び出されるイベント");
-// });
-
-// sock.addEventListener("error", (e) => {
-//   console.log("エラーが発生したときに呼び出されるイベント");
-// });
-
 const btn = document.querySelector("#hello-button");
 const input_value = document.querySelector<HTMLInputElement>("#input");
 btn.addEventListener("click", () => {
