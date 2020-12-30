b4e20ae90471cce0463c2aff7337b2346ca43424
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 07:01:35 +0900

    change simple bash

--- shell.bash
+++ shell.bash
@@ -1,9 +1,16 @@
 
 function po() {
     curl localhost:3000 -H 'Content-Type: application/json' -d '{"type": "one_word", "pos": '$1', "word": "IIIIIIIIIIIIIIII"}'
-    sleep 1
 }
 
 for i in {0..100}; do
     po $((i * 1));
+    # po $((i * 2));
+    # po $((i * 3));
+    # po $((i * 32 * 4));
+    # po $((i *  32 * 4 + 32));
+    # po $((i * 32 * 4 + 32 * 2));
+    # po $((i * 32 * 4 + 32 * 3));
+    # po $((i * 32 * 4 + 32 * 4));
+    sleep 1
 done
