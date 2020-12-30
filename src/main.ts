c9a624092c38ad1d66182ca5801e74c79f9d43e1
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 06:05:50 +0900

    fill size 16, fillRect, 1,1

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,15 @@ sock.addEventListener("message", (message_e: MessageEvent<string>) => {
     const x = (pos % 32) * 16;
     const y = Math.floor(pos / 32);
     console.log(`x: ${x}, y: ${y}`);
-    for (var i = 0; i < word.length; i++) {
+    // for (var i = 0; i < word.length; i++) {
+    for (var i = 0; i < 16; i++) {
       const char = word[i];
       if (char === "O") {
         ctx.fillStyle = "white";
-        ctx.fillRect(x + i, y, 10, 10);
+        ctx.fillRect(x + i, y, 1, 1);
       } else {
         ctx.fillStyle = "black";
-        ctx.fillRect(x + i, y, 10, 10);
+        ctx.fillRect(x + i, y, 1, 1);
       }
     }
     console.log(`fill ends.`);
