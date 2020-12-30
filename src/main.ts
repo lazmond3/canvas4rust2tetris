e759b7a231c42a6633544b5ddf6956a69095b0a7
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 07:00:59 +0900

    reconnector on client

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,41 @@ import { ctx } from "./canvas";
 sock.addEventListener("open", (e) => {
   console.log("接続が開かれたときに呼び出されるイベント");
 });
+function connect() {
+  var ws = new WebSocket("ws://127.0.0.1:5001");
+  ws.onopen = function () {
+    console.log(`connect open on.`);
+  };
 
-sock.addEventListener("message", (message_e: MessageEvent<string>) => {
+  ws.onmessage = function (e) {
+    console.log("Message:", e.data);
+    eventListener(e);
+  };
+
+  ws.onclose = function (e) {
+    console.log(
+      "Socket is closed. Reconnect will be attempted in 1 second.",
+      e.reason
+    );
+    setTimeout(function () {
+      connect();
+    }, 1000);
+  };
+
+  ws.onerror = function (err) {
+    console.log("エラーが発生したときに呼び出されるイベント");
+    console.error(
+      "Socket encountered error: ",
+      (err as any).message,
+      "Closing socket"
+    );
+    ws.close();
+  };
+}
+
+connect();
+
+const eventListener = (message_e: MessageEvent<string>) => {
   console.log("メッセージ : " + `${message_e.data}`);
   const data = JSON.parse(message_e.data);
   if (data.type === "one_word") {
@@ -26,15 +59,39 @@ sock.addEventListener("message", (message_e: MessageEvent<string>) => {
     }
     console.log(`fill ends.`);
   }
-});
+};
 
-sock.addEventListener("close", (e) => {
-  console.log("接続が閉じられたときに呼び出されるイベント");
-});
+// sock.addEventListener("message", (message_e: MessageEvent<string>) => {
+//   console.log("メッセージ : " + `${message_e.data}`);
+//   const data = JSON.parse(message_e.data);
+//   if (data.type === "one_word") {
+//     const pos = data.pos;
+//     const word: string = data.word;
+//     console.log(`one word req: pos: ${pos}, word: ${word}`);
+//     const x = (pos % 32) * 16;
+//     const y = Math.floor(pos / 32);
+//     console.log(`x: ${x}, y: ${y}`);
+//     for (var i = 0; i < 16; i++) {
+//       const char = word[i];
+//       if (char === "O") {
+//         ctx.fillStyle = "white";
+//         ctx.fillRect(x + i, y, 1, 1);
+//       } else {
+//         ctx.fillStyle = "black";
+//         ctx.fillRect(x + i, y, 1, 1);
+//       }
+//     }
+//     console.log(`fill ends.`);
+//   }
+// });
 
-sock.addEventListener("error", (e) => {
-  console.log("エラーが発生したときに呼び出されるイベント");
-});
+// sock.addEventListener("close", (e) => {
+//   console.log("接続が閉じられたときに呼び出されるイベント");
+// });
+
+// sock.addEventListener("error", (e) => {
+//   console.log("エラーが発生したときに呼び出されるイベント");
+// });
 
 const btn = document.querySelector("#hello-button");
 const input_value = document.querySelector<HTMLInputElement>("#input");
