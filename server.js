872cd93febf754851afe782161962d16b1429d05
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-30 16:38:37 +0900

    add bypass req single word request to api server

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const ws_port = 5001;
 // API サーバ
 const api_server = http.createServer((req, res) => {
   // ここにrequestを書いていく
+  const ws_message = make_ws_server();
+
+  if (req.type === "one_word") {
+    const pos = req.position;
+    const word = req.word;
+
+    ws_message.then(({ ws, message }) => {
+      ws.send(JSON.stringify(req));
+    });
+  }
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World\n");
@@ -19,22 +30,29 @@ api_server.listen(api_port, hostname, () => {
   );
 });
 
-// WS_server
-const ws_server = require("ws").Server;
-const s = new ws_server({
-  host: hostname,
-  port: ws_port,
-});
-
-console.log(`start listening... : websocket server`);
-s.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    console.log("Received: " + message);
-
-    if (message === "hello") {
-      ws.send("hello from server");
-    } else if (message === "test") {
-      ws.send("test from server");
-    }
+//  -----------------  WS_server --------------------
+function make_ws_server() {
+  const ws_server = require("ws").Server;
+  const s = new ws_server({
+    host: hostname,
+    port: ws_port,
   });
-});
+
+  console.log(`start listening... : websocket server`);
+
+  return new Promise((resolve, reject) => {
+    s.on("connection", (ws) => {
+      ws.on("message", (message) => {
+        resolve({ ws, message });
+        // console.log("Received: " + message);
+
+        // if (message === "hello") {
+        //   ws.send("hello from server");
+        // } else if (message === "test") {
+        //   ws.send("test from server");
+        // }
+      });
+    });
+  });
+}
+//  -----------------  WS_server --------------------
