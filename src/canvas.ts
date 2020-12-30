e33dd81f56b37100f7bf40248604508c0dfb34a2
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 07:01:30 +0900

    update canvas settings

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,7 +1,13 @@
 const canvas = document.querySelector<HTMLCanvasElement>("#canvas");
+canvas.width = 512;
+canvas.height = 256;
+canvas.style.width = "512px";
+canvas.style.height = "256px";
 
 export const ctx = canvas.getContext("2d");
 
+// ctx.scale(1, 1);
+// ctx.scale(0.5, 0.5);
 // fill green
 ctx.fillStyle = "white";
 // ctx.fillRect(10, 10, 150, 100);
