ae7a03b64e01b9cb44766b3e602d93402530311b
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 06:05:34 +0900

    change fill rect size from 0 to 256

--- src/canvas.ts
+++ src/canvas.ts
@@ -4,8 +4,10 @@ export const ctx = canvas.getContext("2d");
 
 // fill green
 ctx.fillStyle = "white";
-ctx.fillRect(10, 10, 150, 100);
+// ctx.fillRect(10, 10, 150, 100);
+ctx.fillRect(0, 0, 512, 256);
 ctx.strokeStyle = "black";
 // ctx.lineWidth = 1;
 // ctx.shadowBlur = 0;
-ctx.strokeRect(10, 10, 150, 100);
+ctx.fillRect(0, 0, 512, 256);
+// ctx.strokeRect(10, 10, 150, 100);
