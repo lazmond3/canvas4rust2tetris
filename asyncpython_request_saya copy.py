fd1bee168553651e2cf628f5bd684582bf0f6e39
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 09:43:34 +0900

    update async but rugg

--- asyncpython_request_saya copy.py
+++ asyncpython_request_saya copy.py
@@ -3,13 +3,13 @@ import asyncio
 
 # async
 # https://qiita.com/ragnar1904/items/85b81febefd3f3f2899a
-async def post_async(data: str) -> dict:
+async def post_async(data: str, line_number: int):
     loop = asyncio.get_event_loop()
     url = "http://localhost:3000"
     headers = {'content-type': 'application/json'}
     arg = {"headers": headers, "data": data}
-    res = await loop.run_in_executor(None, requests.post, url, data, headers)
-    return res.json()
+    await loop.run_in_executor(None, requests.post, url, data, headers)
+    print("-- line number: {}".format(line_number))
 
 def main_async():
     loop = asyncio.get_event_loop()
@@ -21,10 +21,10 @@ def main_async():
         for line_ in f.readlines():
             line = line_.strip()
             payload = {"type": "one_word", "pos": linenum, "word": line}
-            data.append( json.dumps(payload))
+            data.append( (json.dumps(payload), linenum))
             linenum += 1
 
-    tasks_ = [post_async(d) for d in data]
+    tasks_ = [post_async(d, ln) for d,ln in data]
     tasks = asyncio.gather(
         *tasks_
     )
