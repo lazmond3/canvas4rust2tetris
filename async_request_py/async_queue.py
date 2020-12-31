2ade2f96b38b46cccd9a8d39aa7d43c0fda7e41c
Kazami Ryosuke <JP26446@w022581806348m.local>
2020-12-31 10:14:56 +0900

    delete lines

--- async_request_py/async_queue.py
+++ async_request_py/async_queue.py
@@ -3,13 +3,6 @@ import asyncio
 
 # async
 # https://qiita.com/ragnar1904/items/85b81febefd3f3f2899a
-async def post_async(data: str, line_number: int):
-    loop = asyncio.get_event_loop()
-    url = "http://localhost:3000"
-    headers = {'content-type': 'application/json'}
-    arg = {"headers": headers, "data": data}
-    await loop.run_in_executor(None, requests.post, url, data, headers)
-    print("-- line number: {}".format(line_number))
 
 async def queue_execution(words, callback, parallel=3000):
     loop = asyncio.get_event_loop()
@@ -33,24 +26,6 @@ async def queue_execution(words, callback, parallel=3000):
     tasks = [post_async(queue) for i in range(parallel)]
     return await asyncio.wait(tasks)
 
-def main_async():
-    loop = asyncio.get_event_loop()
-    # 非同期実行タスクを一つのFutureオブジェクトに
-    data = []
-    with open("saya.txt") as f:
-        linenum = 0
-        for line_ in f.readlines():
-            line = line_.strip()
-            linenum += 1
-
-    tasks_ = [post_async(d, ln) for d,ln in data]
-    tasks = asyncio.gather(
-        *tasks_
-    )
-    results = loop.run_until_complete(tasks)
-
-# if __name__ == '__main__':
-#     main_async()
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
